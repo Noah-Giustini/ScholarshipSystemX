ed24d3af6828bcade6555d2777deb1821d613a6c
Noah Giustini <36166847+Noah-Giustini@users.noreply.github.com>
2019-03-23 21:29:42 -0600

    Student View Application

--- src/AdditionalScenes/StudentApplicationViewController.java
+++ src/AdditionalScenes/StudentApplicationViewController.java
@@ -5,51 +5,67 @@
  */
 package AdditionalScenes;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import seng300.Seng300;
 
 /**
  * FXML Controller class
  *
- * @author Kaitlin
+ * @author Noah
  */
 public class StudentApplicationViewController implements Initializable {
 
     @FXML
     private Button backButton;
     @FXML
-    private Button awardButton;
+    private Button acceptButton;
+    @FXML
+    private Button withdrawButton;
     @FXML
     private ScrollPane scrollWindow;
     @FXML
     private TextArea applicationInfo;
-    @FXML
-    private Button refreshButton;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        this.applicationInfo.setText(Seng300.theManager.getCurrentApplication().toString());
     }    
 
     @FXML
-    private void backToViewApplications(ActionEvent event) {
+    private void backToViewApplications(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/seng300/studentMainPage.fxml"));
+        Scene sc = new Scene(root);
+        Stage s = Seng300.mainStage; 
+        s.setTitle("Home");
+        s.setScene(sc);
+        s.show();
     }
 
     @FXML
-    private void award(ActionEvent event) {
+    private void accept(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void withdraw(ActionEvent event) {
+        
     }
 
-    @FXML
-    private void refreshAction(ActionEvent event) {
-    }
+    
     
 }
