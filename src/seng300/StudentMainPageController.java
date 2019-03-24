f88193b3e22e466f54eda6ad8658cdcaea6b6865
Kaitlin <kaitlin.dechastelainfinnigan@gmail.com>
2019-03-23 21:36:54 -0600

    student main im sorry

--- src/seng300/StudentMainPageController.java
+++ src/seng300/StudentMainPageController.java
@@ -7,7 +7,14 @@ package seng300;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleGroup;
 
 /**
  * FXML Controller class
@@ -16,6 +23,129 @@ import javafx.fxml.Initializable;
  */
 public class StudentMainPageController implements Initializable {
 
+    @FXML
+    private ScrollPane currentScholarshipScrollPane;
+    @FXML
+    private RadioButton radioSchol1;
+    @FXML
+    private ToggleGroup ListOfSchols;
+    @FXML
+    private RadioButton radioSchol2;
+    @FXML
+    private RadioButton radioSchol3;
+    @FXML
+    private RadioButton radioSchol4;
+    @FXML
+    private RadioButton radioSchol5;
+    @FXML
+    private Label lblScholarship1;
+    @FXML
+    private Label lbldate1;
+    @FXML
+    private Label lblScholarship2;
+    @FXML
+    private Label lbldate2;
+    @FXML
+    private Label lblScholarship3;
+    @FXML
+    private Label lbldate3;
+    @FXML
+    private Label lblScholarship4;
+    @FXML
+    private Label lbldate4;
+    @FXML
+    private Label lblScholarship5;
+    @FXML
+    private Label lbldate5;
+    @FXML
+    private Label lbldate6;
+    @FXML
+    private Label lblScholarship7;
+    @FXML
+    private Label lbldate7;
+    @FXML
+    private RadioButton radioSchol6;
+    @FXML
+    private RadioButton radioSchol7;
+    @FXML
+    private Label lblScholarship6;
+    @FXML
+    private ScrollPane historyScrollPane;
+    @FXML
+    private Label lblDraft1;
+    @FXML
+    private RadioButton rdoDraft1;
+    @FXML
+    private ToggleGroup ListOfDrafts;
+    @FXML
+    private RadioButton rdoDraft2;
+    @FXML
+    private RadioButton rdoDraft3;
+    @FXML
+    private RadioButton rdoDraft4;
+    @FXML
+    private RadioButton rdoDraft5;
+    @FXML
+    private RadioButton rdoDraft6;
+    @FXML
+    private RadioButton rdoDraft7;
+    @FXML
+    private Button adminMainPageLogoutButton;
+    @FXML
+    private Button viewApplicationsButton;
+    @FXML
+    private Button refreshMainAdminPage;
+    @FXML
+    private Button buttonEditDraft;
+    @FXML
+    private Label schstatuslbl;
+    @FXML
+    private Button buttonEdit;
+    @FXML
+    private ScrollPane historyScrollPane1;
+    @FXML
+    private RadioButton rdoDraft11;
+    @FXML
+    private ToggleGroup ListOfDrafts1;
+    @FXML
+    private RadioButton rdoDraft21;
+    @FXML
+    private RadioButton rdoDraft31;
+    @FXML
+    private RadioButton rdoDraft41;
+    @FXML
+    private RadioButton rdoDraft51;
+    @FXML
+    private RadioButton rdoDraft61;
+    @FXML
+    private RadioButton rdoDraft71;
+    @FXML
+    private Label lblOpenSchol1;
+    @FXML
+    private Label lblOpenSchol2;
+    @FXML
+    private Label lblOpenSchol3;
+    @FXML
+    private Label lblOpenSchol4;
+    @FXML
+    private Label lblOpenSchol5;
+    @FXML
+    private Label lblOpenSchol6;
+    @FXML
+    private Label lblOpenSchol7;
+    @FXML
+    private Label lblDraft2;
+    @FXML
+    private Label lblDraft3;
+    @FXML
+    private Label lblDraft4;
+    @FXML
+    private Label lblDraft5;
+    @FXML
+    private Label lblDraft6;
+    @FXML
+    private Label lblDraft7;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +153,22 @@ public class StudentMainPageController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void openApplicationsWindow(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleRefresh(ActionEvent event) {
+    }
+
+
+    @FXML
+    private void editDraft(ActionEvent event) {
+    }
+
+    @FXML
+    private void editSubmitted(ActionEvent event) {
+    }
     
 }
